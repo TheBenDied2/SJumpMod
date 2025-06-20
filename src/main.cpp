6df1273a46161c7bd071db778f10c8f62a91db58
TheBenDied2 <sts162771@gmail.com>
2025-06-20 11:13:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
-include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/Geode.hpp>
 
-using namespace geode::prelude;
-
-class $modify(MyPlayLayer, PlayLayer) {
-    void update(float dt) {
-        PlayLayer::update(dt);
-
-        // Натискання клавіші S
-        if (getKeyState(KEY_S, false)) {
-            this->pushButton(1, true);
-        }
-
-        // Відпускання клавіші S
-        if (!getKeyState(KEY_S, true)) {
-            this->releaseButton(1, true);
-        }
+class SJumpMod : public geode::Mod {
+public:
+    void onLoad() override {
+        auto gameLayer = geode::GameLayer::get();
+        gameLayer->setKeyDownHandler(geode::Key::S, [this]() {
+            gameLayer->player()->jump();
+        });
     }
 };
+
+static SJumpMod s_jump_mod;
