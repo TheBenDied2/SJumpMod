b4b0b2c15e5b6c3938605f14e1e9df3b1dfb8b82
TheBenDied2 <sts162771@gmail.com>
2025-06-20 09:00:28 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyPlayLayer, PlayLayer) {
+    void update(float dt) {
+        PlayLayer::update(dt);
+
+        // Натискання клавіші S
+        if (getKeyState(KEY_S, false)) {
+            this->pushButton(1, true);
+        }
+
+        // Відпускання клавіші S
+        if (!getKeyState(KEY_S, true)) {
+            this->releaseButton(1, true);
+        }
+    }
+};
